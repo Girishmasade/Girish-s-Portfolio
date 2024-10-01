a56f20c936a3f64d43ee5c5aa5cc8ffb0e725f36
Girishmasade <girishmasade@gmail.com>
2024-10-01 11:44:07 +0530

    Form Validation updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { ContactData } from '../utils/ContactData'
 import emailjs from '@emailjs/browser';
 import toast from 'react-hot-toast'
+import { message } from 'antd';
 
 const notify = () => toast('Form submited successfully', {style: {border: '1px solid black', background: 'rgb(17 24 39)', color: 'white'}});
 
 const Contact = () => {
+
+  const [Disable, setDisable] = useState(true)
+  const [user, setUSer] = useState({
+    text: '',
+    email: '',
+    message: ''
+  })
+  
+  useEffect(() => {
+   if (user.text.length > 0 && user.email.length > 0 && user.message.length > 0) {
+    setDisable(false)
+   } else {
+    setDisable(true)
+   }
+  }, [user])
+  
+
   const form = useRef();
 
   const sendEmail = (e) => {
@@ -50,17 +68,17 @@ const Contact = () => {
         <input type="hidden" name="access_key" value="YOUR_ACCESS_KEY_HERE" />
         <div className="mb-4">
           <label htmlFor="name" className="block text-gray-700">Name:</label>
-          <input type="text" id="name" name="from_name" className="w-full px-3 py-2 border rounded-lg" placeholder="Your Name" required />
+          <input type="text" value={user.text} onChange={(e)=> setUSer({...user, text: e.target.value})} id="name" name="from_name" className="w-full px-3 py-2 border rounded-lg" placeholder="Your Name" required />
         </div>
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700">Email:</label>
-          <input type="email" id="email" name="email" className="w-full px-3 py-2 border rounded-lg" placeholder="Your Email" required />
+          <input type="email" id="email" value={user.email} onChange={(e)=> setUSer({...user, email: e.target.value})} name="email" className="w-full px-3 py-2 border rounded-lg" placeholder="Your Email" required />
         </div>
         <div className="mb-4">
           <label htmlFor="message" className="block text-gray-700">Message:</label>
-          <textarea id="message" name="message" className="w-full px-3 py-2 border rounded-lg" placeholder="Your Message" required></textarea>
+          <textarea id="message" name="message" value={user.message} onChange={(e)=> setUSer({...user, message: e.target.value})} className="w-full px-3 py-2 border rounded-lg" placeholder="Your Message" required></textarea>
         </div>
-        <button type="submit" onClick={notify} value='send' className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600">Submit</button>
+        <button type="submit" onClick={notify} value='send' className={`${Disable ? 'bg-blue-300 cursor-not-allowed' : 'bg-blue-500 hover:bg-blue-600'} w-full text-white py-2 rounded-lg `}>Submit</button>
       </form>
     </div>
   </div>
